82abaded8760e63dbb1575ec809c6278ca227640
yakshesh08 <yaksheshg@gmail.com>
2021-08-17 13:48:57 +0530

    Dice Challenge

---
+++ index.js
@@ -0,0 +1,30 @@
+// alert("Working");
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = ("🚩 Player1 Wins!!!");
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = ("Player2 Wins!!! 🚩")
+}
+else if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").innerHTML = ("Drwa!! Refresh Again")
+}
